b58db1396ec6e1b5c98cf38aaeb59f6578a538bc
Marvin Cayetano <cayetanomarvin@gmail.com>
2017-11-13 02:05:14 +0000

    BOOTSTRAPPED HOME PAGE

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'clients', to: 'pages#clients'
+
   resources :contacts
+
   get 'login', to: 'pages#login'
   get 'products', to: 'pages#products'
   get 'services', to: 'pages#services'
