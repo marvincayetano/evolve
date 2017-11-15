1e986e0c5ef34906b4a6913085028be34870336a
Marvin Cayetano <cayetanomarvin@gmail.com>
2017-11-15 12:42:42 +0000

    Added comments

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,17 +1,21 @@
 class ContactsController < ApplicationController
 
+  # GET request to /contact-us
+  # Show new contact form page
   def new
     @contact = Contact.new
   end
 
+  # POST request /contacts
   def create
+    # Instantiate new Contact Object
     @contact = Contact.new(contact_params)
-    if @contact.save
+    if @contact.save # Saves to database and returns true or false
       name = params[:contact][:name]
       email = params[:contact][:email]
       body  = params[:contact][:comments]
-      ContactMailer.contact_email(name, email, body).deliver
-      flash[:success] = "Message sent."
+      ContactMailer.contact_email(name, email, body).deliver # Calls contact_email function from ContactMailer class
+      flash[:success] = "Message sent."# Changable :success
       redirect_to new_contact_path
     else
       flash[:danger] = @contact.errors.full_messages.join(", ")
@@ -20,6 +24,7 @@ class ContactsController < ApplicationController
   end
 
   private
+    # Collects data from form
     def contact_params
       params.require(:contact).permit(:name, :email, :comments)
     end
