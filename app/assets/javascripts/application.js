b022bc9a11db8c0f05190670eb120790ca4da49c
Marvin Cayetano <cayetanomarvin@gmail.com>
2017-11-17 23:48:51 +0000

    Added text fields for sign up view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,18 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+(function($){
+// Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+})(jQuery);
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,2 @@
 class Company < Activerecord::Base
-
 end
