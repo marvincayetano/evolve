ac76c0885c98c1b1bd504132201c98a9d5765d11
Marvin Cayetano <cayetanomarvin@gmail.com>
2017-11-18 20:13:39 +0000

    Fixed navbar scroll jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,17 +16,17 @@
 //= require turbolinks
 //= require_tree .
 
-(function($){
 // Collapse Navbar
+$(document).on('turbolinks:load', function(){
   var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
+    if ($('#mainNav').offset().top > 100) {
+      $('#mainNav').addClass('navbar-shrink');
     } else {
-      $("#mainNav").removeClass("navbar-shrink");
+      $('#mainNav').removeClass('navbar-shrink');
     }
   };
   // Collapse now if page is not at top
   navbarCollapse();
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
-})(jQuery);
+});
