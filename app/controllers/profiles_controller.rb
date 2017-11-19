43edc4874ab85daa8cf9dc34a8893f949a695cb7
Marvin Cayetano <cayetanomarvin@gmail.com>
2017-11-19 19:42:38 +0000

    Added filters and locking down pages

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,8 @@
 class ProfilesController < ApplicationController
+  # DEVISE ACTION USED TO LOCKDOWN PAGES
+  before_action :authenticate_user!
+  before_action :only_current_user
+
   def new
     @profile = Profile.new    
   end
@@ -6,12 +10,13 @@ class ProfilesController < ApplicationController
   def create
     # User who is currently logged in 
     # this comes from the url
+    # PATH NEEDS THIS CODE id: params[:user_id]
     @user = User.find( params[:user_id] )
     # Create profile link to user
     @profile = @user.build_profile( profile_params )
     if @profile.save 
       flash[:success] = "Profile Updated"
-      redirect_to user_path( params[:user_id] )
+      redirect_to user_path(id: params[:user_id] )
     else
       render action: :new
     end
@@ -22,9 +27,26 @@ class ProfilesController < ApplicationController
     @profile = @user.profile
   end
 
+  def update
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+    if  @profile.update_attributes( profile_params )
+      flash[:success] = "Profile updated!"
+      redirect_to user_path(id: params[:user_id])
+    else
+      render action: :edit
+    end
+  end
+
   private
   # WHITELISTING THE FORM SO HACKER CANT ADD FORM FIELD
-  def profile_params
-    params.require(:profile).permit(:avatar, :first_name, :last_name, :job_title, :phone_number, :description) 
-  end
+  # USES FOR MASS ASSIGN
+    def profile_params
+      params.require(:profile).permit(:avatar, :first_name, :last_name, :job_title, :phone_number, :description) 
+    end
+
+    def only_current_user
+      @user = User.find( params[:user_id] )
+      redirect_to(root_url) unless @user == current_user
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  # DEVISE ACTION USED TO LOCKDOWN PAGES
+  before_action :authenticate_user!
 
   def show
     @user = User.find( params[:id] ) 
