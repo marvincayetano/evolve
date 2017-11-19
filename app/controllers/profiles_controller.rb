0a1afc981fb0dcec1e8fe19fd9b5d99611e9031c
Marvin Cayetano <cayetanomarvin@gmail.com>
2017-11-19 18:50:38 +0000

    Added edit profile view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -17,6 +17,11 @@ class ProfilesController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+  end
+
   private
   # WHITELISTING THE FORM SO HACKER CANT ADD FORM FIELD
   def profile_params
