997471d10fe1a6e256153ac470671fb206d9cf94
Marvin Cayetano <cayetanomarvin@gmail.com>
2017-11-19 02:44:03 +0000

    Added routes for profiles

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,22 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   # USER LOGIN
+  # Devise is a gem for user authentication
   devise_for :users
 
-  get 'contacts/new'
+  # USER PROFILE
+  # resource :profile is singular which mean each user
+  # has only one profile each, has many will be profiles
+  resources :users do
+    resource :profile
+  end
+
 
   get 'about', to: 'pages#about'
   get 'clients', to: 'pages#clients'
 
   # CONTACTS
+  get 'contacts/new'
   resources :contacts, only: [:create]
   get 'contact-us', to: 'contacts#new', as: 'new_contact'
 
