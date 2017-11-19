288c44c99e0316b49fdbadba80030d1840f61c19
Marvin Cayetano <cayetanomarvin@gmail.com>
2017-11-19 23:53:30 +0000

    Changed navbar

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -47,6 +47,6 @@ class ProfilesController < ApplicationController
 
     def only_current_user
       @user = User.find( params[:user_id] )
-      redirect_to(root_url) unless @user == current_user
+      redirect_to(root_path) unless @user == current_user
     end
 end
