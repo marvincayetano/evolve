d3eb6196224ca75714155b4a721659ec2ef0daf4
Marvin Cayetano <cayetanomarvin@gmail.com>
2017-11-23 07:48:41 +0000

    Added company_id to user

--- db/schema.rb
+++ db/schema.rb
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20171120000909) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "user_type_id",           default: 3
+    t.integer  "company_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
