6c3a3083cf64b1bb0950ad5edc7bbcc06eb4355b
Marvin Cayetano <cayetanomarvin@gmail.com>
2017-12-04 01:06:04 +0000

    Added list of user posts show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   # DEVISE ACTION USED TO LOCKDOWN PAGES
   before_action :authenticate_user!
 
+  def index
+
+  end
+
   def show
     @user = User.find( params[:id] ) 
   end
